923cbc461816e832bf39daa21bd82ddeb87c55c3
Teevio <stephen@simplecast.com>
2019-08-08 08:23:30 -0400

    all the checks

--- simplecast/simplecast.php
+++ simplecast/simplecast.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Simplecast Embed
  * Plugin URI: https://www.simplecast.com
- * Description: Display content using a shortcode to insert in a page or post. eg: [simplecast-embed src="[simplecast/episode/share/url]"]
+ * Description: Display content using a shortcode to insert in a page or post. eg: [simplecast-embed src="[simplecast/episode/embed/link]"]
  * Version: 0.1
  * Text Domain: simplecast-embed
  * Author: Simplecast
@@ -10,15 +10,34 @@
  */
 
 function simplecast_embed($atts) {
-  $curlSession = curl_init();
-  curl_setopt($curlSession, CURLOPT_URL, 'https://api.simplecast.com/oembed?url=' . $atts["src"]);
-  curl_setopt($curlSession, CURLOPT_BINARYTRANSFER, true);
-  curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+  if (!isset($atts['src'])) {
+    return '[simplecast-embed error="src attribute needs to be set"]';
+  }
 
-  $jsonData = json_decode(curl_exec($curlSession));
-  curl_close($curlSession);
+  $response = wpcom_vip_file_get_contents('https://api.simplecast.com/oembed?url=' . rawurlencode($atts["src"]));
 
-  return $jsonData->html;
+  if ($response == false) {
+    return '[simplecast-embed error="Could not find episode"]';
+  }
+
+  $jsonData = json_decode($response, true);
+
+  if (!$jsonData || !isset($jsonData['html']) || !is_string($jsonData['html'])) {
+    return '[simplecast-embed error="Could not get iframe html"]';
+  }
+
+  $whitelist = array(
+    'iframe' => array(
+      'frameborder' => array(),
+      'height' => array(),
+      'scrolling' => array(),
+      'src' => array(),
+      'title' => array(),
+      'width' => array()
+    )
+  );
+
+  return wp_kses($jsonData['html'], $whitelist);
 }
 
 add_shortcode('simplecast-embed', 'simplecast_embed');
